12fdaa1b02290459003bf2a46e98755c144e4914
nshevchenko <nicola.shevchenko@gmail.com>
2015-04-18 09:38:39 +0100

    API call now with lyrics

--- test.js
+++ test.js
@@ -24,23 +24,37 @@ var router = express.Router();              // get an instance of the express Ro
 router.get('/', function(req, res) {
     //res.json({ message: 'hooray! welcome to our api!' });   
     var first_response = res;
-
-    rest.json('http://api.musixmatch.com/ws/1.1/track.search?q=back%20to%20december&q_artist=taylor%20swift&f_has_lyrics=1&apikey=36238a5e7169c1acedc538f970fffb34').on('complete', function(response) { 
-     // console.log(response); 
-
+    var track_name = "Drone";
+    var art_name = "Muse";
+    console.log("got here 2");
+    rest.json('http://api.musixmatch.com/ws/1.1/track.search?q=' + track_name + '&q_artist=' + art_name + ' &f_has_lyrics=1&apikey=36238a5e7169c1acedc538f970fffb34')
+    .on('complete', function(response_track) { 
+        console.log("got here 1");
+    //console.log(response); 
+    var json_track_info = JSON.parse(response_track);
+    //console.log(json_track_info.message.body.track_list[0].track_rating);
      //var second_response = "doing something" . reponse;
-
-     	rest.json('http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=15953433&apikey=36238a5e7169c1acedc538f970fffb34', function(response_2){
-     			//first_response.send(response_2);
+        var track_id = json_track_info.message.body.track_list[0].track.track_id;
+        // console.log(json_track_info);
+     	rest.json('http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=' + track_id + '&apikey=36238a5e7169c1acedc538f970fffb34')
+            .on('complete', function(response_2){
+            var lyrics = JSON.parse(response_2).message.body.lyrics.lyrics_body;
+            
+            console.log(lyrics);
+            first_response.send("" + lyrics);
      	});
-
-     	 first_response.send(response);
-
+         
+        
+     	
 	})
 	.on('error', function(err) { 
 	      console.log('An error occurred:' + err); 
 	 });
 });
+
+//async
+//angular/react
+//jquery
 
 // more routes for our API will happen here
 
